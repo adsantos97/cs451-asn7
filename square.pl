89e273d1edf916ed13e196addf96c30a50b6fcdb
adsantos97 <asantos@cs.loyola.edu>
2018-05-01 09:59:09 -0400

    printing out a square

--- square.pl
+++ square.pl
@@ -1,36 +1,29 @@
 % Arizza Santos
 % CS451 ASN7
 
-%magic_square([
-%    row(_,_,_),
-%    row(_,_,_),
-%    row(_,_,_)
-%]).
+% Magic Constant -> sum of each row, column, diagonal
+% M = n((n^2)+1)/2
+% n = 3 -> three by three
+% M = 3((3^2)+1)/2
+% M = 15
 
-%generate([
-%    N1, N2, N3,
-%    N4, N5, N6,
-%    N7, N8, N9
-%]) :- 
-%Numbers = [1,2,3,4,5,6,7,8,9].
+generate(L, L1) :- permutate(L, L1).
 
 remove(X, [X|Y], Y).
 remove(X, [H|Y], [H|Z]) :- remove(X, Y, Z).  
 
-%permutate([], []).
-%permutate([X|Xs], L) :- permutate(Xs, L1), remove(X, L, L1). 
+permutate([], []).
+permutate([X|Xs], L) :- permutate(Xs, L1), remove(X, L, L1). 
 
+square([
+    row(1,2,3),
+    row(4,5,6),
+    row(7,8,9)
+]).
 
-
-%numbers([1,2,3,4,5,6,7,8,9]).
-
-%takeout(X,[X|R],R).  
-%takeout(X,[F |R],[F|S]) :- takeout(X,R,S).
-
-%perm([X|Y],Z) :- perm(Y,W), takeout(X,Z,W).  
-%perm([],[]).
-
-print_magic_square([A|B]) :- !,
+print_square([A|B]) :- !,
     write(A), nl,
     print_magic_square(B).
-print_magic_square([]).
+print_square([]).
+
+main(X) :- square(X), print_square(X).
