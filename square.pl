6cd40dea34e5a505eec863822c5cba624f22449f
adsantos97 <asantos@cs.loyola.edu>
2018-05-01 10:42:47 -0400

    starting generate

--- square.pl
+++ square.pl
@@ -7,7 +7,13 @@
 % M = 3((3^2)+1)/2
 % M = 15
 
+%generate([], []).
+%generate([L|Ls], L1) :- permutate(L, L1), generate(Ls, L1).
+
 generate(L, L1) :- permutate(L, L1).
+
+app([], Xs, Xs).
+app([X|Xs], Ys, [X|Zs]) :- app(Xs, Ys, Zs).
 
 remove(X, [X|Y], Y).
 remove(X, [H|Y], [H|Z]) :- remove(X, Y, Z).  
@@ -16,14 +22,14 @@ permutate([], []).
 permutate([X|Xs], L) :- permutate(Xs, L1), remove(X, L, L1). 
 
 square([
-    row(1,2,3),
-    row(4,5,6),
-    row(7,8,9)
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
 ]).
 
 print_square([A|B]) :- !,
     write(A), nl,
-    print_magic_square(B).
+    print_square(B).
 print_square([]).
 
 main(X) :- square(X), print_square(X).
