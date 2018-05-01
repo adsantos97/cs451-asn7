3ea4f2a1e24eef0141cb2bd737eb69c1ec54bc3b
adsantos97 <asantos@cs.loyola.edu>
2018-05-01 18:16:16 -0400

    first 3 goals

--- appends.pl
+++ appends.pl
@@ -4,4 +4,7 @@
 app([], Xs, Xs).
 app([X|Xs], Ys, [X|Zs]) :- app(Xs, Ys, Zs).
 
-second(X, Ys) :- app([_],[X|_], Ys).
+second(X, Ys) :- app([_], [X|_], Ys).
+lastElm(X, Ys) :- app(_, [X],Ys).
+
+allButLast(Xs, Ys) :- app(Xs, [_], Ys).
