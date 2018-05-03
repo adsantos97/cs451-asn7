e388aebd794b343e97accb7b9bae3d94b0947a4c
adsantos97 <asantos@cs.loyola.edu>
2018-05-02 23:30:54 -0400

    finished comments

--- appends.pl
+++ appends.pl
@@ -11,25 +11,53 @@ app([X|Xs], Ys, [X|Zs]) :- app(Xs, Ys, Zs).
 %    the query in English form is
 %    "Is Element second in the List?"
 second(X, Ys) :- app([_], [X|_], Ys).
+% Tests:
+% second(X, [1,2,3]). -> "What is the second element X in [1,2,3]?"
+% second(2, [1,2,3,4]). -> "Is 2 the second element in the [1,2,3,4]?"
+% second(2, X). -> "2 is the second element in what list?"
 
 % lastElm(?Element, ?List)
 %    If Element and List are instantiated,
 %    the query in English form is
 %    "Is Element the last element of the List?"
 lastElm(X, Ys) :- app(_, [X],Ys).
+% Tests:
+% lastElm(X, [1,2,3]). -> "What is the last element in [1,2,3]?"
+% lastElm(3, [1,2,3]). -> "Is 3 the last element in [1,2,3]?"
+% lastElem(3, X). -> "3 is the last element in what list?"
 
 % allButLast(?Sublist, ?List)
 %   If Sublist and List are instantiated,
 %   the query in English form is
 %   "Is Sublist all but the last of List?"
 allButLast(Xs, Ys) :- app(Xs, [_], Ys).
+% Tests:
+% "What is all but the last of [1,2,3]?"
+% allButLast(X, [1,2,3]).
+% allButLast([1,2], [1,2,3]). -> "Is [1,2] all but the last element of [1,2,3]?"
+% allButLast([1,2], X). -> "[1,2] is all but the last of what list?"
 
 % concat_three(?SubList, ?ResultList)
 %    Three copies of SubList concatenated
 %    together, giving ResultList.
 concat_three(Xs, Ys) :- app([Xs], [Xs,Xs], Ys).
+% Tests:
+% "What is the sublist concatenated three time to create
+% [[1,2],[1,2],[1,2]]?"
+% concat_three(X, [[1,2],[1,2],[1,2]]).
+% "Is [1,2] concatenated three times [[1,2],[1,2],[1,2]]?"
+% concat_three([[1,2],[1,2],[1,2]]).
+% "What is the result of concatenating three copies of [1,2]?"
+% concat_three([1,2], X).
 
 % ins(?A, ?X, ?Y)
-%    Whether a list Y is formed by inserting A
-%    somewhere in a list X.
+%    Whether a list Y is formed by inserting 
+%    element A somewhere in a list X.
 ins(A, X, Y) :- app(L1, L2, X), app(L1, [A|L2], Y).
+% Tests:
+% "What is inserted into [1,2,3] to produce [1,2,3,5]?"
+% ins(A, [1,2,3], [1,2,3,5]).
+% "What is the list produced when 5 is inserted to [1,2,3]?"
+% ins(5, [1,2,3], X). 
+% "Is 5 inserted to [1,2,3], [1,2,3,5]?"
+% ins(5, [1,2,3], [1,2,3,5]).
--- square.pl
+++ square.pl
@@ -1,6 +1,7 @@
 % Arizza Santos
 % CS451 ASN7
 
+% Assignment Notes:
 % Magic Constant -> sum of each row, column, diagonal
 % M = n((n^2)+1)/2
 % n = 3 -> three by three
@@ -8,28 +9,44 @@
 % M = 15
 % permutations? 9 digits, so 9! -> 362880
 
+% generate(+List, -ProcessedList)
+%    Generates a ProcessedList from List
+%    using permutate.
 generate(L, L1) :- permutate(L, L1).
 
+% test(+List)
+%   Tests a given List to see if it is
+%   a Magic Square.
 test([
     N1, N2, N3,
     N4, N5, N6,
     N7, N8, N9
 ]) :-
-    sumOfList(15, [N1, N2, N3]),
-    sumOfList(15, [N4, N5, N6]),
-    sumOfList(15, [N7, N8, N9]),
-    sumOfList(15, [N1, N4, N7]),
-    sumOfList(15, [N2, N5, N8]),
-    sumOfList(15, [N3, N6, N9]),
-    sumOfList(15, [N1, N5, N9]),
-    sumOfList(15, [N7, N5, N3]).
+    sumOf(15, [N1, N2, N3]),
+    sumOf(15, [N4, N5, N6]),
+    sumOf(15, [N7, N8, N9]),
+    sumOf(15, [N1, N4, N7]),
+    sumOf(15, [N2, N5, N8]),
+    sumOf(15, [N3, N6, N9]),
+    sumOf(15, [N1, N5, N9]),
+    sumOf(15, [N7, N5, N3]).
 
-sumOfList(0, []).
-sumOfList(X, [L|Ls]) :-  sumOfList(Y, Ls), X is L + Y. 
+% sumOf(?Sum, ?List)
+%    If Sum and List are instantiated,
+%    the query in English form is
+%    "Is Sum the sum of the List?" 
+sumOf(0, []).
+sumOf(X, [L|Ls]) :-  sumOf(Y, Ls), X is L + Y. 
 
+% remove(+Element, +List, -ProcessedList)
+%    Removes the Element of List, giving ProcessedList.
 remove(X, [X|Y], Y).
 remove(X, [H|Y], [H|Z]) :- remove(X, Y, Z).  
 
+% permutate(?List, ?ProcessedList)
+%    If List and ProcessedList are instantiated,
+%    the query in English form is
+%    "Is List a permutation of ProcessedList?"
 permutate([], []).
 permutate([X|Xs], L) :- permutate(Xs, L1), remove(X, L, L1). 
 
@@ -39,12 +56,16 @@ square([
     7,8,9
 ]).
 
+% print_square(+List)
+%    A list (in this case, of 9 elements) is printed out
+%    displaying 3 elements on each line.
 print_square([A, B, C|D]) :- !,
     write(A), write(' '), write(B), write(' '), write(C), nl,
     print_square(D).
 print_square([]).
 
-main(X) :- 
+% driver code
+main:- 
     square(X), 
     generate(X, L),
     test(L),
